c3621aa47ddf96a8c1f3edffb6386f041243e793
VvNicol <150023181+VvNicol@users.noreply.github.com>
2023-12-20 11:18:55 +0100

    Add files via upload

---
+++ nrojlla.Arrays4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace nrojlla.Arrays4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el tamaño del vector:");
+            int NumeroSolicitado = Convert.ToInt32(Console.ReadLine());
+
+            int[] limite = new int[NumeroSolicitado];                    
+            Random r = new Random();   
+            
+            for (int i = 0; i < NumeroSolicitado; i++)
+            {   
+                limite[i] = r.Next(0, 101);
+                
+            }
+            Console.WriteLine("Resultado: ");
+
+            Array.Sort(limite);
+
+            foreach (int num in limite)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
